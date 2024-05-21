a6229e69fc1b1b67b3a6ab6c51ff0a91b9bd29b4
sapphir0v <6.sapphirov.6@bk.ru>
2024-05-21 21:31:36 +0300

    refactor code after flake8 and mypy

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,9 +1,10 @@
 import pytest
-from src.processing import key_value_output, decreasing_dates
+
+from src.processing import decreasing_dates, key_value_output
 
 
 @pytest.fixture
-def unique_list():
+def unique_list() -> list:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -12,21 +13,33 @@ def unique_list():
     ]
 
 
-@pytest.mark.parametrize("key_state, expected_result", [("EXECUTED", [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-    ]),
-                                                        ("CANCELED", [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])])
-def test_key_value_output(unique_list, key_state, expected_result):
+@pytest.mark.parametrize(
+    "key_state, expected_result",
+    [
+        (
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+def test_key_value_output(unique_list: list, key_state: str, expected_result: list) -> None:
     assert key_value_output(unique_list, key_state) == expected_result
 
 
-def test_decreasing_dates(unique_list):
+def test_decreasing_dates(unique_list: list) -> None:
     assert decreasing_dates(unique_list) == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
